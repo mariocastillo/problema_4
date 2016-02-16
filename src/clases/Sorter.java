715c09a14a7447505ea6eab7134c807b763b7d19
Mario Castillo <marioe.castillo93@gmail.com>
2016-02-16 13:59:58 -0500

    ordena lista
    se agrego codigo en sorter para que fuera comparacion en lista

--- src/clases/Sorter.java
+++ src/clases/Sorter.java
@@ -1,5 +1,7 @@
 package clases;
 
+import java.util.ArrayList;
+
 import interfaces.IComparisonStrategy;
 
 
@@ -16,16 +18,37 @@ public  class Sorter {
 	}
 	
 
-	public Student Sort(Student a,Student b){
-		if (strategy.comparisonf(a, b)<0) {
-			return a;
+//	public Student Sort(Student a,Student b){
+//		if (strategy.comparisonf(a, b)<0) {
+//			return a;
+//		}
+//		else if (strategy.comparisonf(a, b)>0) {
+//			return b;
+//		}
+//		else {
+//			return a;
+//			}
+//  	}
+	
+	public ArrayList<Student> Sort(ArrayList<Student> a){
+		
+		for (int i = 0; i < a.size(); i++) {
+			for (int j = i+1; j < a.size(); j++) {
+				if (strategy.comparisonf(a.get(i), a.get(j))>0) {
+					a.add(i,a.get(j));
+					a.remove(j+1);
+				}
+			}			
 		}
-		else if (strategy.comparisonf(a, b)>0) {
-			return b;
-		}
-		else {
-			return a;
-		}
+		return a;
+		
+		
+//		if (strategy.comparisonf(a, b)<0) {
+//			return a;
+//		}
+//		else {
+//			return b;
+//		}
 		
 	}
 
